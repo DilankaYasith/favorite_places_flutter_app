4c86ca60e5c53b5d1d829353e5229133c88b8636
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-08-08 00:36:31 +0530

    added the camera effect

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +16,7 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   @override
   void dispose() {
@@ -24,10 +27,12 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
 
     Navigator.of(context).pop();
   }
@@ -50,7 +55,11 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
               ),
             ),
             const SizedBox(height: 10),
-            const ImageInput(),
+            ImageInput(
+              onPickImage: (File image) {
+                _selectedImage = image;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
